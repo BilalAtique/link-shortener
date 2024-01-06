afeaa26d6f66e311f3cd1cb5ac11c69fd281f15d
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-06 19:36:43 +0500

    fix: remove fullname from jwt

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -47,7 +47,6 @@ userSchema.methods.generateAccessToken = function () {
       _id: this._id,
       email: this.email,
       username: this.username,
-      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET!,
     {
