78459e6c66d03c593b12b5254cbd9f1cc981cb49
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-06 19:21:25 +0500

    feat: add user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
