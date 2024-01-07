1fed595f065323085d0e6b9409591eedaf5b257c
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-07 15:27:51 +0500

    feat: add middlware for cors and json

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,11 @@
 import express, { Express } from "express";
+import cors from "cors";
 
 const app: Express = express();
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
 
 export default app;
