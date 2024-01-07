9c0f8fefda7380016773836e457b8effdeb8109b
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-07 18:43:10 +0500

    feat: add user routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,18 @@
 import express, { Express } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 
 const app: Express = express();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
+//routes import
+import userRouter from "./routes/user.routes";
+
+//routes declaration
+app.use("/api/users", userRouter);
 
 export default app;
