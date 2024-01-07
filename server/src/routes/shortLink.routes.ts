3e9983edb80a85f86cdf0bafb044daa4c590975c
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-07 20:57:41 +0500

    feat: add route to delete link

--- server/src/routes/shortLink.routes.ts
+++ server/src/routes/shortLink.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { createShortLink, getUserShortLinks } from "../controllers/shortLink.controller";
+import {
+  createShortLink,
+  deleteLinkById,
+  getUserShortLinks,
+} from "../controllers/shortLink.controller";
 
 const router = Router();
 
 router.route("/").post(verifyJWT, createShortLink);
 
-router.route("/").get(verifyJWT, getUserShortLinks)
+router.route("/").get(verifyJWT, getUserShortLinks);
+
+router.route("/").delete(verifyJWT, deleteLinkById);
 
 export default router;
