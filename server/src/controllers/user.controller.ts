38dbbcee365a652bf3b9ebba7af324740d180fb7
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-07 18:43:59 +0500

    feat: add controller to register user

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { User } from "../models/user.model";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { fullName, email, password } = req.body;
+  console.log(req.body);
+  if ([fullName, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({ email });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email already exists");
+  }
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+});
+
+export { registerUser };
