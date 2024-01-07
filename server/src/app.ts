8363af6207cb1eb4848578696366e40b7460fa17
Bilal Atique <74999164+BilalAtique@users.noreply.github.com>
2024-01-07 20:27:29 +0500

    feat: add shortLink router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,21 @@
 import express, { Express } from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes";
+import shortLinkRouter from "./routes/shortLink.routes";
 
 //routes declaration
 app.use("/api/users", userRouter);
+
+app.use("/api/short-link", shortLinkRouter);
 
 export default app;
