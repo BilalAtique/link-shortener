0c9e51d95f993edae5fa027cfdb70c23ef7fee21
Abdur Rehman Rehan <rehannoaman13@gmail.com>
2024-01-11 23:03:59 +0500

    feat: Created Register page

---
+++ client/src/Pages/auth/Register.jsx
@@ -0,0 +1,94 @@
+import register from "../../assets/register.svg";
+
+const Register = () => {
+  return (
+    <div>
+      <div className="flex h-screen">
+        {/* <!-- Left Pane --> */}
+
+        <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
+          <div className="max-w-md text-center">
+            <img src={register} alt="Register" />
+          </div>
+        </div>
+
+        {/* <!-- Right Pane --> */}
+        <div className="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
+          <div className="max-w-md w-full p-6">
+            <h1 className="text-3xl font-semibold mb-6 text-black text-center">
+              Sign Up
+            </h1>
+            <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">
+              Join now to transform your lengthy links{" "}
+            </h1>
+
+            <div className="mt-4 text-sm text-gray-600 text-center">
+              <p>With email</p>
+            </div>
+            <form action="#" method="POST" className="space-y-4">
+              {/* <!-- Your form elements go here --> */}
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="fullname"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Full name
+                </label>
+                <input
+                  type="text"
+                  id="fullname"
+                  name="fullname"
+                  className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 text-sm text-gray-600 text-center">
+              <p>Already have an account? </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
