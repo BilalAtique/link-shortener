3881be610532483ceac007cc764b1138c9a2aae7
Abdur Rehman Rehan <rehannoaman13@gmail.com>
2024-01-12 18:29:42 +0500

    feat: added homepage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom/dist";
 import Login from "./Pages/auth/Login";
 import Register from "./Pages/auth/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
 
 function Auth() {
   return <Navigate to="/login" />;
@@ -12,6 +13,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route
