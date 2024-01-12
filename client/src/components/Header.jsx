83b6b83b940d0bf7f937427120db1dbb00dd26b7
Abdur Rehman Rehan <rehannoaman13@gmail.com>
2024-01-13 02:02:52 +0500

    refactor: changed the color scheme

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,12 @@ import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-center h-[7vh] bg-pink-800 text-gray-200 px-4">
+    <header className="flex items-center justify-center h-[7vh] bg-black text-white px-4">
       <img src={logo} alt="logo" className="w-24 h-12" />
       <nav className="w-[100%]">
         <ul className="flex px-7 justify-end">
           <li className="px-4 text-xl font-bold">Home</li>
-          <li className="px-4 text-xl font-bold">Sign In</li>
+          <li className="px-4 text-xl font-bold">Log Out</li>
         </ul>
       </nav>
     </header>
--- client/src/components/UrlInput.jsx
+++ client/src/components/UrlInput.jsx
@@ -1,20 +1,20 @@
 const UrlInput = () => {
   return (
-    <div className="bg-gray-900 text-white h-[30%] ">
+    <div className="bg-gray-800 text-white h-[30%] ">
       <div className="flex flex-col  items-center h-full">
-        <h2 className="my-6 text-4xl text-pink-800 font-semibold">Short URL</h2>
+        <h2 className="my-6 text-4xl text-pink-700 font-semibold">Short URL</h2>
         <div className="text-2xl">
           <input
-            className="px-4 h-11 outline-none bg-gray-200 rounded-l-md text-gray-800"
+            className="px-4 h-11 w-96 outline-none bg-gray-200 rounded-s-full text-gray-800"
             type="text"
-            placeholder="Enter the Link Here..."
+            placeholder="Enter the Link Here...    "
           />
-          <button className="px-4 h-11 bg-pink-800 rounded-r-md outline-none hover:bg-pink-700">
+          <button className="px-7 h-11 bg-pink-700 rounded-r-full outline-none hover:bg-pink-900">
             Get your Link
           </button>
         </div>
-        <div className="p-5 text-sm">
-          <p></p>
+        <div className="p-5 text-2xl">
+          <p>shorten link</p>
         </div>
       </div>
     </div>
