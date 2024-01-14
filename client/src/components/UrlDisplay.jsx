6e2ae1f4c6419f095abff811b07939dd65531a00
Abdur Rehman Rehan <rehannoaman13@gmail.com>
2024-01-14 19:45:50 +0500

    refactor: Display users url
    Display the links shorten by the user
    some design improvements

--- client/src/components/UrlDisplay.jsx
+++ client/src/components/UrlDisplay.jsx
@@ -1,68 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import NewLink from "./NewLink";
+import Cookies from "universal-cookie";
 
 const UrlDisplay = () => {
-  const links = [
-    {
-      _id: 1,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 2,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 3,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 4,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 5,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 6,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 7,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 8,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 9,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-    {
-      _id: 10,
-      shortenedLink: "https://swiftshrink.com/123456",
-      originalLink: "https://www.google.com",
-      clicks: 10,
-    },
-  ];
+  const cookies = new Cookies();
+  const fetchLinks = async () => {
+    const response = await fetch("http://127.0.0.1:3000/api/short-links", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${cookies.get("accessToken")}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch links");
+    }
+
+    return await response.json();
+  };
+
+  const { data, isPending } = useQuery({
+    queryKey: ["userLinks"],
+    queryFn: fetchLinks,
+  });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex w-full p-12 justify-center ">
       <div className="p-10 text-white w-[80%] shadow-2xl shadow-black rounded-3xl">
@@ -81,13 +46,13 @@ const UrlDisplay = () => {
                 links --> array of links
                 array of links - map - NewLink component props
              */}
-            {links.map((link, index) => (
+            {data?.data?.userLinks.map((link, index) => (
               <NewLink
                 key={link._id}
                 index={index + 1}
-                shortenedLink={link.shortenedLink}
+                shortenedLink={link.shortLink}
                 originalLink={link.originalLink}
-                clicks={link.clicks}
+                clicks={link.visits}
               />
             ))}
           </tbody>
