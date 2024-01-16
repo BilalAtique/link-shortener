6b2aa7389f006c0113ee9e66b460f23088ec8e8f
Abdur Rehman Rehan <rehannoaman13@gmail.com>
2024-01-16 18:44:39 +0500

    Added Delete Button

--- client/src/components/NewLink.jsx
+++ client/src/components/NewLink.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 import { toast } from "react-toastify";
+import { IoCopySharp } from "react-icons/io5";
+import { MdDeleteSweep } from "react-icons/md";
 
 const NewLink = ({ index, shortenedLink, originalLink, clicks }) => {
   return (
@@ -8,17 +10,36 @@ const NewLink = ({ index, shortenedLink, originalLink, clicks }) => {
       <td className="border px-4 py-2 flex justify-between ">
         {shortenedLink}
         <button
-          className="bg-green-400 px-4 py-2"
+          className=" px-4 py-2"
           onClick={() => {
             navigator.clipboard.writeText(shortenedLink);
             toast.success("Copied!");
           }}
         >
-          Copy
+          <IoCopySharp />
         </button>
       </td>
       <td className="border px-4 py-2">{originalLink}</td>
       <td className="border px-4 py-2">{clicks}</td>
+      <td className="border px-4 py-2">
+        <button
+          className="text-red-700 py-2"
+          onClick={() => {
+            toast.success("Deleted", {
+              style: {
+                color: "red",
+                fontWeight: "bold",
+              },
+              progressStyle: {
+                backgroundColor: "red",
+              },
+              icon: <MdDeleteSweep size={24} color="black" />,
+            });
+          }}
+        >
+          <MdDeleteSweep size={24} />
+        </button>
+      </td>
     </tr>
   );
 };
